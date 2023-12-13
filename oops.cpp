c1ea043551bef2e7c59922a8c05816152d3c5146
akshita <akshita25103@gmail.com>
2023-12-13 23:37:07 +0530

    ad readme

---
+++ oops.cpp
@@ -0,0 +1,21 @@
+// swaping number
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,temp;
+    cout<<"enter 1st number"<<endl;
+    cin>>a;
+    cout<<"enter 2nd number"<<endl;
+    cin>>b;
+    cout<<"before swapping"<<endl;
+    cout<b<"numbers are: "<<a<< "and" <<b<<endl;
+    cout <<"after swappping"<<endl;
+
+    a = temp;
+    temp = b;
+    b = a;
+    cout <<"numbers are"<< a <<" and "<<b;
+
+
+}
